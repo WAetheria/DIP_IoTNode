a1ebd2de15794045114c805871a376fdc7884f10
WAetheria <wl.aetheria@gmail.com>
2025-10-28 21:21:10 +0800

    feat: copied some test code to see if the JPEG camera capture is correct from ChatGPT

--- include/nodes/node_test.h
+++ include/nodes/node_test.h
@@ -2,18 +2,43 @@
 #define TEST_NODE
 
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 #include "camera.h"
 #include "device.h"
 
-Device resistor = Device(35, DeviceMode::ANALOG_INPUT);
+const char* ssid = "ssid";
+const char* password = "password";
 
-void setup(){
-    Serial.begin(115200);
+WebServer server(80);
+
+void handleCapture() {
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb) {
+    server.send(500, "text/plain", "Camera capture failed");
+    return;
+  }
+
+  server.sendHeader("Content-Type", "image/jpeg");
+  server.send_P(200, "image/jpeg", (const char *)fb->buf, fb->len);
+  esp_camera_fb_return(fb);
 }
 
-void loop(){
-    Serial.println(resistor.readInput());
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500); Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+
+  server.on("/capture", handleCapture);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
 }
 
 #endif
