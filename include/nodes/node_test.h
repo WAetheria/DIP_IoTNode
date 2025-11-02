692ad64de6bca353c75d1894cc4b0270ca03b90d
WAetheria <wl.aetheria@gmail.com>
2025-11-02 18:09:31 +0800

    fix: minor

--- include/nodes/node_test.h
+++ include/nodes/node_test.h
@@ -14,31 +14,31 @@ const char* password = "password";
 WebServer server(80);
 
 void handleCapture() {
-  camera_fb_t *fb = esp_camera_fb_get();
-  if (!fb) {
-    server.send(500, "text/plain", "Camera capture failed");
-    return;
-  }
+	camera_fb_t *fb = esp_camera_fb_get();
+	if (!fb) {
+	server.send(500, "text/plain", "Camera capture failed");
+	return;
+	}
 
-  server.sendHeader("Content-Type", "image/jpeg");
-  server.send_P(200, "image/jpeg", (const char *)fb->buf, fb->len);
-  esp_camera_fb_return(fb);
+	server.sendHeader("Content-Type", "image/jpeg");
+	server.send_P(200, "image/jpeg", (const char *)fb->buf, fb->len);
+	esp_camera_fb_return(fb);
 }
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
-  }
-  Serial.println(WiFi.localIP());
+	Serial.begin(115200);
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {
+	delay(500); Serial.print(".");
+	}
+	Serial.println(WiFi.localIP());
 
-  server.on("/capture", handleCapture);
-  server.begin();
+	server.on("/capture", handleCapture);
+	server.begin();
 }
 
 void loop() {
-  server.handleClient();
+	server.handleClient();
 }
 
 #endif
